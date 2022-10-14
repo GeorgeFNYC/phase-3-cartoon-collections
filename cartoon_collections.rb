8757256c677a906ba7e0fbcb8ede6a82358cecf5
George Fernandez <fernandezg704@gmail.com>
2022-10-14 13:08:44 -0400

    commit tree 384e5f4b892dc8ad84962513050d13b6375b2f60

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves array # code an argument here
   # Your code here
+  array.map.with_index(1){ |dwarf, i| puts "#{i}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
+
+def summon_captain_planet array# code an argument here
   # Your code here
+  array.map {|hero| puts"#{hero}!".capitalize }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls array# code an argument here
   # Your code here
+  array.map {|word| puts"#{word}"}
 end
 
 def find_the_cheese# code an argument here
